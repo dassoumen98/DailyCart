24b160c576873d09f7200b841e60f5655a0c53d3
Soumen Das <dassoumen9832@gmail.com>
2025-08-18 23:09:13 +0530

    feat:add navbar

---
+++ client/src/context/appContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext ,useState } from "react"
+import { useNavigate } from "react-router-dom"
+export const AppContext =createContext()
+
+
+export const AppContextProvider = ({ children }) => {
+    const navigate =useNavigate()
+    const [user ,setUser] = useState(true)
+    const[isSeller, setIsSeller] = useState(false)
+    const [showUserLogin, setShowUserLogin]= useState(false)
+
+    let value ={navigate , user, setUser, isSeller, setIsSeller ,setShowUserLogin  }
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
