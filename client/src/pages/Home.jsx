b335eee7d49307790800cdcf16b75fe17174f2a6
Soumen Das <dassoumen9832@gmail.com>
2025-08-20 23:17:04 +0530

    feat:add mainbanner

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner'
+
+export default function Home() {
+  return (
+    <div className='mt-10'>
+    <MainBanner />
+      
+    </div>
+  )
+}
