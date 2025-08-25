d374b87946f76e5bdd446734c62bec2b6080e3c3
Soumen Das <dassoumen9832@gmail.com>
2025-08-25 15:45:45 +0530

    feat: add AllProducts page and integrate search functionality in Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Footer from './components/Footer'
 
 
 import Home from './pages/Home'
+import AllProducts from './pages/AllProducts'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import Login from './components/Login'
@@ -27,6 +28,7 @@ const {showUserLogin}=useAppContext()
      <div className={ `${isSellerPath ? "" :"px-6 md:px-16 lg:px-24 xl:px-32"}`}>
      <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<AllProducts />} />
      </Routes>
      </div>
 
